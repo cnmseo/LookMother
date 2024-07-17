b8de73e4282ba44edfac1e08f81319162a4f7f1b
cnmseo <145701128+cnmseo@users.noreply.github.com>
2024-07-17 18:06:49 +0700

    Update br.js

--- br.js
+++ br.js
@@ -1,18 +1,1 @@
-var _paq = window._paq = window._paq || [];
-
-_paq.push(['setCustomDimension', 1, location.hostname]);
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-  var u="//web.ems.ac/";
-  _paq.push(['setTrackerUrl', u+'matomo.php']);
-  _paq.push(['setSiteId', '1']);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.async=true;
-  g.src=u+'matomo.js'; 
-  s.parentNode.insertBefore(g,s);
-})();
-
-setTimeout(function() {
-  window.location.href="https://ems.ac/";
-}, 500);
+window.location.href="https://ems.ac/";
